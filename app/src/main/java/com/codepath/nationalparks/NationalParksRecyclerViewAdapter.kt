db690fbd3d1c933849ad449fa985f7d2d8b7153a
Kajus Buikus <kajusbuikus@gmail.com>
2026-02-10 14:10:30 -0500

    Added walkthrough GIF

--- app/src/main/java/com/codepath/nationalparks/NationalParksRecyclerViewAdapter.kt
+++ app/src/main/java/com/codepath/nationalparks/NationalParksRecyclerViewAdapter.kt
@@ -3,33 +3,29 @@ package com.codepath.nationalparks
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.codepath.nationalparks.R.id
+import com.bumptech.glide.Glide
 
-/**
- * [RecyclerView.Adapter] that can display a [NationalPark] and makes a call to the
- * specified [OnListFragmentInteractionListener].
- */
 class NationalParksRecyclerViewAdapter(
     private val parks: List<NationalPark>,
     private val mListener: OnListFragmentInteractionListener?
 ) : RecyclerView.Adapter<NationalParksRecyclerViewAdapter.ParkViewHolder>() {
 
-    // Inflate the item layout from XML
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ParkViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.fragment_national_park, parent, false)
         return ParkViewHolder(view)
     }
 
-    // ViewHolder class holds references to all UI elements inside the list item layout
     inner class ParkViewHolder(val mView: View) : RecyclerView.ViewHolder(mView) {
         var mItem: NationalPark? = null
 
-        // TODO: Step 4a - Add references for remaining views from XML
-        val mParkName: TextView = mView.findViewById(id.park_name) as TextView
-        val mParkDescription: TextView = mView.findViewById(id.park_description) as TextView
+        val mParkName: TextView = mView.findViewById(R.id.park_name)
+        val mParkLocation: TextView = mView.findViewById(R.id.park_location)
+        val mParkDescription: TextView = mView.findViewById(R.id.park_description)
+        val mParkImage: ImageView = mView.findViewById(R.id.park_image)
 
         override fun toString(): String {
             return mParkName.toString() + " '" + mParkDescription.text + "'"
@@ -39,24 +35,20 @@ class NationalParksRecyclerViewAdapter(
     override fun onBindViewHolder(holder: ParkViewHolder, position: Int) {
         val park = parks[position]
 
-        // TODO: Step 4b - Bind the park data to the views
         holder.mItem = park
-        holder.mParkName.text = park.name
-        holder.mParkDescription.text = park.description
+        holder.mParkName.text = park.name ?: ""
+        holder.mParkLocation.text = park.location ?: ""
+        holder.mParkDescription.text = park.description ?: ""
 
-        // TODO: Step 4c - Use Glide to load the first image
+        Glide.with(holder.mView)
+            .load(park.imageUrl)
+            .centerInside()
+            .into(holder.mParkImage)
 
-
-        // Sets up click listener for this park item
         holder.mView.setOnClickListener {
-            holder.mItem?.let { park ->
-                mListener?.onItemClick(park)
-            }
+            holder.mItem?.let { p -> mListener?.onItemClick(p) }
         }
     }
 
-    // Tells the RecyclerView how many items to display
-    override fun getItemCount(): Int {
-        return parks.size
-    }
+    override fun getItemCount(): Int = parks.size
 }
